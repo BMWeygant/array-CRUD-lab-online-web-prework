1e8578b1483c5b7fdf60ff6ba47fcb99d97bfff3
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-24 04:27:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+   []
 end
 
 def create_an_array
-  
+["wow", 1, "am", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+create_an_array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+create_an_array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
